5e66518e7f5e18bbe5c126e1a5c4aea79a39f43c
VasiliyMelnik64 <vasiliymelnik64@gmail.com>
2023-03-02 03:01:43 +0100

    added refactoring

--- Creational/Factory method/main.py
+++ Creational/Factory method/main.py
@@ -1,34 +1,39 @@
 from abc import abstractmethod
 
 
-class Service:
+class IService:
     def create_api():
         pass
 
 
 class ServiceCreator:
     @abstractmethod
-    def create_service() -> Service:
+    def create_service() -> IService:
+        # might me their own logic
         pass
 
 
 class AuthServiceCreator(ServiceCreator):
-    def create_service(self) -> Service:
+    def create_service(self) -> IService:
+        # might me their own logic
         return AuthService()
 
 
 class FetchDataServiceCreator(ServiceCreator):
-    def create_service(self) -> Service:
+    def create_service(self) -> IService:
+        # might me their own logic
         return FetchDataService()
 
 
-class AuthService(Service):
+class AuthService(IService):
     def create_api(self) -> None:
+        # the rest logic
         print('Auth API created')
 
 
-class FetchDataService(Service):
+class FetchDataService(IService):
     def create_api(self) -> None:
+        # the rest logic
         print('FetchData API created')
 
 
